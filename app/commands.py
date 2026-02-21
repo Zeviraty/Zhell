fc4700e6d610a2c7781a592b7e241a6aa0c56c7c
zeviraty <zeviraty@gmail.com>
2026-02-21 20:50:07 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -1,7 +1,13 @@
+import sys
+
 def which(argv):
-    print("command: "+argv[0])
+    sys.stdout.write("command: "+argv[0])
     return 0
 
 def shell_exit(argv):
     if len(argv) < 2: exit(0)
     exit(argv[1])
+
+def echo(argv):
+    sys.stdout.write(' '.join(argv[1:]) + '\n')
+    return 0
