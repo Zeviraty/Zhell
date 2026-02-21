b11ed7c816dbebe82ae6b5d7d5449ce979f12203
zeviraty <zeviraty@gmail.com>
2026-02-21 20:46:02 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -1,3 +1,7 @@
 def which(argv):
     print("command: "+argv[0])
     return 0
+
+def shell_exit(argv):
+    if len(argv) < 2: exit(0)
+    exit(argv[1])
--- app/main.py
+++ app/main.py
@@ -1,28 +1,29 @@
 import sys
 from . import commands 
+from typing import Callable
 
-BUILTINS = {
-    "which": commands.which
+BUILTINS: dict[str,Callable] = {
+    "which": commands.which,
+    "exit": commands.shell_exit,
 }
 
-def get_command(cmd: list[str]) -> tuple:
+def get_command(cmd: list[str]) -> tuple[bool,Callable[[list[str]],int]]:
     if cmd[0] in BUILTINS.keys():
         return (True,BUILTINS[cmd[0]])
     else:
-        return (False,{})
+        return (False,lambda x: exit(x[0]))
 
 def main():
     while True:
         sys.stdout.write("$ ")
 
-        uin = input()
-        cmd = get_command(uin.split(" "))
+        uin = input().split(" ")
+        cmd = get_command(uin)
 
         if cmd[0] == True:
-            pass
+            cmd[1](uin)
         else:
-            sys.stdout.write(f"{uin}: command not found\n")
-
+            sys.stdout.write(f"{uin[0]}: command not found\n")
 
 if __name__ == "__main__":
     main()
