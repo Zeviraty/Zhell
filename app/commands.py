c5d1a5bef654f8c8296f767a1a76b7c279c79344
zeviraty <zeviraty@gmail.com>
2026-02-21 21:08:54 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -1,5 +1,18 @@
 import sys
 from typing import Callable
+import os
+
+def find_command(cmd: str) -> tuple[bool, str]:
+    path = os.environ.get("PATH")
+    if path == None: print("PATH could not be found"); exit(1)
+
+    path = path.split(os.pathsep)
+    for directory in path:
+        files = os.listdir(directory)
+        if cmd in files:
+            path = os.path.join(directory,cmd)
+            if os.access(path, os.X_OK): return (True, path)
+    return (False, "")
 
 def which(argv):
     sys.stdout.write("command: "+argv[0]+'\n')
@@ -24,8 +37,13 @@ def shell_type(argv):
     if argv[1] in BUILTINS.keys():
         sys.stdout.write(argv[1]+' is a shell builtin\n')
         return 0
-    else:
-        sys.stdout.write(argv[1]+': not found\n')
-        return 1
+
+    path = find_command(argv[0])
+    if path[0] == True:
+        sys.stdout.write(argv[1]+' is '+path[1]+'\n')
+        return 0
+
+    sys.stdout.write(argv[1]+': not found\n')
+    return 1
 
 BUILTINS["type"] = shell_type
