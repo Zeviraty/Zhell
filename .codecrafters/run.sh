e48efdc4973d0a3a296bef64a62a9568e572e1ee
zeviraty <zeviraty@gmail.com>
2026-02-27 13:33:40 +0100

    codecrafters submit [skip ci]

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#
-# This script is used to run your program on CodeCrafters
-#
-# This runs after .codecrafters/compile.sh
-#
-# Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit on failure
+set -e
 
-exec uv run --quiet -m app.main "$@"
+SCRIPT_DIR="$(dirname "$0")"
+
+PYTHONSAFEPATH=1 PYTHONPATH="$SCRIPT_DIR" exec uv run \
+  --project "$SCRIPT_DIR" \
+  --quiet \
+  -m app.main \
+  "$@"
