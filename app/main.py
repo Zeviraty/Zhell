976b181d69d25b45a387ffca70dd2c97349a2170
zeviraty <zeviraty@gmail.com>
2026-02-28 13:31:38 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -89,6 +89,8 @@ def completer(text: str, state: int):
         name = text
         parts = text.split(os.sep)
         while '' in parts: parts.remove('')
+        if os.path.isdir(os.sep.join(parts)) and (not os.sep.join(parts).endswith('/')):
+            return os.sep.join(parts)+'/'
         if os.path.isdir(os.sep.join(parts)) and os.sep.join(parts) != '.':
             path = os.sep.join(parts); name = ''
         elif len(parts) > 1:
